0e338f86d3c4bbea782fa5c9ed68ebc11e61405f
pulkit paliwal <20d100021@iitb.ac.in>
2024-05-05 18:03:47 +0530

    fixed cuda device resnet_reproduce.py

--- Code/resnet_reproduce.py
+++ Code/resnet_reproduce.py
@@ -99,8 +99,8 @@ def train(resnet, num_classes = 100, num_epochs = 100, mode = "matryoshka", load
         for i, data in tqdm.tqdm(enumerate(trainloader, 0), total=len(trainloader)):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
-            inputs.to(device)
-            labels.to(device)
+            inputs = inputs.to(device)
+            labels = labels.to(device)
             # zero the parameter gradients
             optimizer.zero_grad()
             outputs_ = resnet(inputs)
@@ -129,8 +129,8 @@ def train(resnet, num_classes = 100, num_epochs = 100, mode = "matryoshka", load
             correct = 0   
             for j, data in enumerate(valloader):
                 images, labels = data
-                images.to(device)
-                labels.to(device)
+                inputs = inputs.to(device)
+                labels = labels.to(device)
                 outputs = resnet(images)
                 predicted = classifiers[i].predict(outputs[:, :min(2**(i+1), cl_embed_size)])
                 total += labels.size(0)
@@ -164,8 +164,8 @@ with torch.no_grad():
         total = 0
         for data in testloader:
             images, labels = data
-            images.to(device)
-            labels.to(device)
+            inputs = inputs.to(device)
+            labels = labels.to(device)
             outputs = net(images)
             predicted = best_classifiers[i].predict(outputs[:, :min(2**(i+1), 1000)])
             total += labels.size(0)
